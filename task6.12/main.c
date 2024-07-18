eee79c52831cdb6ae810424d29a6a11696372706
SnuffyKernel <Snuffy1@yandex.ru>
2024-07-19 06:30:56 +0700

    1

--- task6.12/main.c
+++ task6.12/main.c
@@ -1,17 +1,30 @@
-#include <stdio.h>
-#include "parser.h"
+#include "function_phone_book/function.h"
 
-int main()
-{
-    char str[MAX_SIZE];
+void start(struct Contacts* contacts) {
+    printf("------------------------\n\t1 - addContact\n\t2 - delContact\n\t3 - listContact\n\t4 - exit\n------------------------\n");
 
-    if (fgets(str, MAX_SIZE, stdin) == NULL)
-    {
-        printf("ERROR");
-        return 1;
+    int c = 0;
+    scanf("%d", &c);
+    getchar();
+
+    if (c <= 0 || c > 4) {
+        error("ICORRECT VALUE");
+    } else if (c == 1) {
+        addContact(contacts);
+    } else if (c == 2) {
+        delContact(contacts);
+    } else if ( c == 3 ) {
+        listContact(contacts);
+    } else if ( c == 4 ) {
+        exit(0);
     }
+}
 
-    printf("%d\n", count(str));
+int main() {
+    struct Contacts contacts;
+    initContacts(&contacts);
 
-    return 0;
+    while (1) {
+        start(&contacts);
+    }
 }
--- task6.3/main.c
+++ task6.3/main.c
@@ -1,30 +1,17 @@
-#include "function_phone_book/function.h"
+#include <stdio.h>
+#include "parser.h"
 
-void start(struct Contacts* contacts) {
-    printf("------------------------\n\t1 - addContact\n\t2 - delContact\n\t3 - listContact\n\t4 - exit\n------------------------\n");
+int main()
+{
+    char str[MAX_SIZE];
 
-    int c = 0;
-    scanf("%d", &c);
-    getchar();
-
-    if (c <= 0 || c > 4) {
-        error("ICORRECT VALUE");
-    } else if (c == 1) {
-        addContact(contacts);
-    } else if (c == 2) {
-        delContact(contacts);
-    } else if ( c == 3 ) {
-        listContact(contacts);
-    } else if ( c == 4 ) {
-        exit(0);
+    if (fgets(str, MAX_SIZE, stdin) == NULL)
+    {
+        printf("ERROR");
+        return 1;
     }
-}
 
-int main() {
-    struct Contacts contacts;
-    initContacts(&contacts);
+    printf("%d\n", count(str));
 
-    while (1) {
-        start(&contacts);
-    }
+    return 0;
 }
